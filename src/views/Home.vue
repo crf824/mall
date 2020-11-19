40931c903dd103c85ad5c1526d7669e596f5ca46
cairunfeng <782693305@qq.com>
2020-11-19 16:39:00 +0800

    初始化项目

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+      <van-button color="linear-gradient(to top, #4bb0ff, #6149f6)">
+        渐变色按钮
+      </van-button>
+      <van-button type="primary" block>块级元素</van-button>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
